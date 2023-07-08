eee0eeb7bc41e24ba3693fe5407cfb26a9d09147
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-07-08 18:03:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-primary-color flex justify-center bg-sec-color">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto"],
+    },
+    extend: {},
+    colors: {
+      "primary-color": "#3730a3",
+      "sec-color": "#f1f5f9",
+    },
+  },
+  plugins: [],
+};
