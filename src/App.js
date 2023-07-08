d547bad880daf6a9fa9027ab4f5c85c75eba0ecf
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-07-08 18:12:52 +0530

    initial testing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="text-primary-color flex justify-center bg-sec-color">
-      hello
+      My name is abhi
     </div>
   );
 }
 
 export default App;
+// https://wonderful-babka-545bad.netlify.app/
